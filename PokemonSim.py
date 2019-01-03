9ebf790962ef1124c445be44219fdd29969fd130
Cuppa <jli@ucvts.org>
2019-01-03 15:43:14 +0000

    Added the Pokemon Class, Beginning to create battle function

---
+++ PokemonSim.py
@@ -0,0 +1,41 @@
+import pygame
+import random
+import time
+pname 1 = input("What is the first Trainer's name?")
+pname 2 = input("And what is the second Trainer's name?")
+print(f"Welcome, {pname1} and {pname2}, to Pokemon Battle Simulator, Super Special Python Edition! You will be prompted shortly to choose your team of 3 Pokemon, each with a competitively viable moveset and setup.")
+time.sleep(5)
+class Pokemon:
+    def __init__(self, level, ability, nature, type, item, hpmax, hpcurrent, atk, defs, spatk, spdef,spd, moves, status)
+    self.name = name
+    self.level = level
+    self.ability = ability
+    self.nature = nature
+    self.type = type
+    self.item = item
+    self.hpmax = hpmax
+    self.hpcurrent = hpcurrent
+    self.atk = atk
+    self.defs = defs
+    self.spatk = spatk
+    self.spdef = spdef
+    self.spd = spd
+    self.moves = moves
+    self.status = status
+def damage ():
+  if move.atktype = phys:
+    damagephys = ((((((((42)*chosenmove.power)*(self.atk/opponent.defs))/50)+2)*random.randfloat(.88,1.12))
+    if self.status = brn:
+      newdamagephys = damagephys/2
+  elif move.atktype = spec:
+   damagespec = ((((((((42)*chosenmove.power)*(self.spatk/opponent.spdef))/50)+2)*random.randfloat(.88,1.12))
+  else:
+    dmg = 0
+
+Pokemon List: {
+
+
+
+
+}
+
